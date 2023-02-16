8015299601c9031e079b2469f6b1e324997cfe6c
Facundo Vergara <fakumax@gmail.com>
2023-02-16 14:18:54 -0300

    Fix Change to select

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,29 +2,57 @@ import React from 'react';
 import Input from './commons/Input';
 
 const Sidebar = ({ options, setOptions }) => {
-  const onDataChange = (event) => {
+  const onDataChange = (e) => {
     setOptions((options) => ({
       ...options,
-      [event.target.name]: event.target.value,
+      [e.target.name]: e.target.value,
     }));
   };
   const handlechange = (e) => {
-    console.log('event', e);
     setOptions((options) => ({
       ...options,
-      [e.target.text]: e.target.value,
+      [e.target.getAttribute('field')]: {
+        ...options[e.target.getAttribute('field')],
+        [e.target.getAttribute('property')]: e.target.value,
+      },
     }));
   };
+  // const handleQrOptions = (e) => {
+  //   setOptions((options) => ({
+  //     ...options,
+  //     qrOptions: {
+  //       ...options.qrOptions,
+  //       [e.target.name]: e.target.value,
+  //     },
+  //   }));
+  // };
+  const handleQrOptions = (e) => {
+    setOptions((options) => ({
+      ...options,
+      qrOptions: {
+        ...options.qrOptions,
+        [e.target.name]: e.target.value,
+      },
+    }));
+  };
+  const handleCornerStyle = (e) => {
+    console.log('e', e.target.id);
+    // setOptions((options) => ({
+    //   ...options,
+    //   qrOptions: {
+    //     ...options.qrOptions,
+    //     [e.target.name]: e.target.value,
+    //   },
+    // }));
+  };
+
   return (
     <div>
       <div>
         <label htmlFor='files'>Image File : </label>
         <input type='file' id='files' />
       </div>
-      {/* <div>
-        <label htmlFor='width'>Width :</label>
-        <input type='number' id='width' name='width' onChange={onDataChange} />
-      </div> */}
+
       <Input
         type='number'
         text='Width : '
@@ -47,9 +75,15 @@ const Sidebar = ({ options, setOptions }) => {
         setOptions={setOptions}
       />
       <div>
-        <h3>Dots Option</h3>
-        <label htmlFor='dotsOptions'>Dots Style: </label>
-        <select name='dotsOptions' id='dotsOptions' onChange={handlechange}>
+        <h3>Dots Options</h3>
+        <label htmlFor='type'>Dots Style: </label>
+        <select
+          id='dot-type'
+          name='dot-type'
+          property='type'
+          field='dotsOptions'
+          onChange={handlechange}
+        >
           <option value='square'>Square</option>
           <option value='dots'>Dots</option>
           <option value='rounded'>Rounded</option>
@@ -59,9 +93,37 @@ const Sidebar = ({ options, setOptions }) => {
         </select>
       </div>
       <div>
-        <label htmlFor='dotscolor'>Dots Color : </label>
-        <input type='color' id='dotscolor' />
+        <label htmlFor='color'>Dots Color : </label>
+        <input
+          type='color'
+          id='dot-color'
+          name='dot-color'
+          property='color'
+          field='dotsOptions'
+          onChange={handlechange}
+        />
       </div>
+      {/*********************/}
+      <div>
+        <h3>Corners Square Options</h3>
+        <label htmlFor='cornerSquare'>Corners Square Style : </label>
+        <select
+          name='cornerSquare'
+          id='cornerSquaress'
+          onChange={handleCornerStyle}
+        >
+          <option value=''>None</option>
+          <option value='square'>Square</option>
+          <option value='dot'>Dot</option>
+          <option value='extra-rounded'>Extra rounded</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor='color'>Corners Square Color : </label>
+        <input type='color' id='color' name='color' onChange={handlechange} />
+      </div>
+      {/*********************/}
+
       <div>
         <label htmlFor=''>Background color : </label>
         <input type='file' />
@@ -74,26 +136,24 @@ const Sidebar = ({ options, setOptions }) => {
       <div>
         <h3>QR Options</h3>
         <label htmlFor='mode'>Mode: </label>
-        <select>
-          <option value='numeric'>numeric</option>
-          <option value='alphanumeric'>Alphanumeric</option>
-          <option value='byte'>Byte</option>
-          <option value='kanji'>Kanji</option>
+        <select name='mode' id='mode' onChange={handleQrOptions}>
+          <option value='Numeric'>numeric</option>
+          <option value='Alphanumeric'>Alphanumeric</option>
+          <option value='Byte'>Byte</option>
+          <option value='Kanji'>Kanji</option>
         </select>
       </div>
       <div>
-        <label
-          htmlFor='correctionlevel'
-          name='qrOptions'
-          onChange={onDataChange}
+        <label htmlFor='errorCorrectionLevel'>Error Correction Level :</label>
+        <select
+          id='errorCorrectionLevel'
+          name='errorCorrectionLevel'
+          onChange={handleQrOptions}
         >
-          Error Correction Level :
-        </label>
-        <select id='qrOptions' onChange={onDataChange}>
-          <option value='l'>L</option>
-          <option value='m'>M</option>
-          <option value='q'>Q</option>
-          <option value='h'>H</option>
+          <option value='L'>L</option>
+          <option value='M'>M</option>
+          <option value='Q'>Q</option>
+          <option value='H'>H</option>
         </select>
       </div>
     </div>
